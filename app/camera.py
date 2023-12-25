8741553794f8aac078969c6dc96bbb9c5505ddaa
Vadim Fedorov <vadim.fedorov@gmail.com>
2023-12-25 14:10:01 +0200

    Workaround for camera out of memory errors.

--- app/camera.py
+++ app/camera.py
@@ -1,5 +1,9 @@
 import io
+import logging
+import sys
+import os
 from threading import Condition
+import traceback
 
 from picamera2 import Picamera2
 from picamera2.encoders import JpegEncoder
@@ -30,30 +34,37 @@ class Camera(BaseCamera):
 
     @staticmethod
     def frames():
-        with Picamera2() as camera:
-            mode = camera.sensor_modes[CAMERA_SENSOR_MODE]
-            camera.configure(camera.create_video_configuration(
-                sensor={
-                    "output_size": mode["size"],
-                    "bit_depth": mode["bit_depth"]
-                },
-            ))
-            camera.set_controls(CAMERA_CONTROLS_NIGHT if Camera.night_mode\
-                                else CAMERA_CONTROLS_DAY)
-            output = StreamingOutput()
-            camera.start_recording(JpegEncoder(), FileOutput(output))
-            while True:
-                with output.condition:
-                    output.condition.wait()
-                    frame = output.frame
-                yield frame
-                if Camera.mode_toggle_requested:
-                    Camera.mode_toggle_requested = False
-                    print("Performing night mode toggle")
-                    if Camera.night_mode:
-                        camera.set_controls(CAMERA_CONTROLS_NIGHT)
-                    else:
-                        camera.set_controls(CAMERA_CONTROLS_DAY)
+        try:
+            with Picamera2() as camera:
+                mode = camera.sensor_modes[CAMERA_SENSOR_MODE]
+                camera.configure(camera.create_video_configuration(
+                    sensor={
+                        "output_size": mode["size"],
+                        "bit_depth": mode["bit_depth"]
+                    },
+                ))
+                camera.set_controls(CAMERA_CONTROLS_NIGHT if Camera.night_mode
+                                    else CAMERA_CONTROLS_DAY)
+                output = StreamingOutput()
+                camera.start_recording(JpegEncoder(), FileOutput(output))
+                while True:
+                    with output.condition:
+                        output.condition.wait()
+                        frame = output.frame
+                    yield frame
+                    if Camera.mode_toggle_requested:
+                        Camera.mode_toggle_requested = False
+                        if Camera.night_mode:
+                            camera.set_controls(CAMERA_CONTROLS_NIGHT)
+                        else:
+                            camera.set_controls(CAMERA_CONTROLS_DAY)
+        except OSError as e:
+            # We are getting "Cannot allocate memory" errors from the camera
+            # from time to time. TODO: Investigate, locate memory leak.
+            # Workaround: die and let systemd restart the service.
+            logging.error(f"Camera error: {e}\n{traceback.format_exc()}")
+            logging.error("Terminating the service")
+            os._exit(1)
 
     @staticmethod
     def toggle_mode():
